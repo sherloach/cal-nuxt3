8c820a02c9dce8a76923e1077b2e6b3cc3e0d355
Hayden Ngo <hoango1607@gmail.com>
2023-12-07 23:03:40 +1030

    style: config global css vars

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import animate from 'tailwindcss-animate'
 import { fontFamily } from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
+const subtleColor = "#E5E7EB";
 module.exports = {
   darkMode: ['class'],
 
@@ -19,39 +20,79 @@ module.exports = {
         sans: ['Inter', 'Inter fallback', ...fontFamily.sans],
       },
       colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
+        emphasis: 'var(--cal-bg-emphasis)',
+        default: 'var(--cal-bg, white)',
+        subtle: 'var(--cal-bg-subtle)',
+        muted: 'var(--cal-bg-muted)',
+        inverted: 'var(--cal-bg-inverted)',
+        info: 'var(--cal-bg-info)',
+        success: 'var(--cal-bg-success)',
+        attention: 'var(--cal-bg-attention)',
+        error: 'var(--cal-bg-error)',
+        darkerror: 'var(--cal-bg-dark-error)',
+        black: '#111111',
+        brand: {
+          default: 'var(--cal-brand,#111827)',
+          emphasis: 'var(--cal-brand-emphasis,#101010)',
+          subtle: 'var(--cal-brand-subtle,#9CA3AF)',
+          accent: 'var(--cal-brand-accent,white)',
         },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+        gray: {
+          50: '#F9FAFB',
+          100: '#F3F4F6',
+          200: '#E5E7EB',
+          300: '#D1D5DB',
+          400: '#9CA3AF',
+          500: '#6B7280',
+          600: '#4B5563',
+          700: '#374151',
+          800: '#1F2937',
+          900: '#111827',
         },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
+        darkgray: {
+          50: '#101010',
+          100: '#1c1c1c',
+          200: '#2b2b2b',
+          300: '#444444',
+          400: '#575757',
+          500: '#767676',
+          600: '#a5a5a5',
+          700: '#d6d6d6',
+          800: '#e8e8e8',
+          900: '#f3f4f6',
         },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
+      },
+      borderColor: {
+        emphasis: 'var(--cal-border-emphasis, #9CA3AF)',
+        default: 'var(--cal-border, #D1D5DB)',
+        subtle: `var(--cal-border-subtle, ${subtleColor})`,
+        muted: 'var(--cal-border-muted, #F3F4F6)',
+        booker: `var(--cal-border-booker, ${subtleColor})`,
+        error: 'var(--cal-border-error, #AA2E26)',
+      },
+      textColor: {
+        emphasis: 'var(--cal-text-emphasis, #111827)',
+        default: 'var(--cal-text, #374151)',
+        subtle: 'var(--cal-text-subtle, #6B7280)',
+        muted: 'var(--cal-text-muted, #9CA3AF)',
+        inverted: 'var(--cal-text-inverted, white)',
+        info: 'var(--cal-text-info, #253985)',
+        success: 'var(--cal-text-success, #285231)',
+        attention: 'var(--cal-text-attention, #73321B)',
+        error: 'var(--cal-text-error, #752522)',
+        brand: 'var(--cal-brand-text,\'white\')',
+      },
+      fill: {
+        emphasis: 'var(--cal-text-emphasis, #111827)',
+        default: 'var(--cal-text, #374151)',
+        subtle: 'var(--cal-text-subtle, #6B7280)',
+        muted: 'var(--cal-text-muted, #9CA3AF)',
+        inverted: 'var(--cal-text-inverted, white)',
+        info: 'var(--cal-text-info, #253985)',
+        success: 'var(--cal-text-success, #285231)',
+        attention: 'var(--cal-text-attention, #73321B)',
+        error: 'var(--cal-text-error, #752522)',
+        brand: 'var(--cal-brand-text)',
       },
       borderRadius: {
         lg: 'var(--radius)',
